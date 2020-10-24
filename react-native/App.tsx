d8f2b7108a2e29b98cc7a0ba1a27473e792b225c
David Hew-wing <dhew67@gmail.com>
2020-10-24 14:31:03 -0400

    project set-up

--- react-native/App.tsx
+++ react-native/App.tsx
@@ -15,7 +15,7 @@ const styles = StyleSheet.create({
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up App.tsx taso start working on your app!</Text>
       <StatusBar />
     </View>
   );
